a666675b8baa4420f6a667d93f166e014d2947e5
Barba2k2 <lorenzo.dz@hotmail.com>
2024-05-11 15:05:46 -0300

    Code fix

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,19 +1,27 @@
+//ignore_for_file: deprecated_member_use
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:asuka/asuka.dart' as asuka;
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Cuidapet',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ScreenUtilInit(
+      designSize: const Size(390, 844),
+      builder: (_, __) => MaterialApp.router(
+        title: 'Cuidapet',
+        debugShowCheckedModeBanner: false,
+        builder: (context, child){
+          return asuka.builder(context, child);
+        },
+        theme: ThemeData(primarySwatch: Colors.blue),
+        routeInformationParser: Modular.routeInformationParser,
+        routerDelegate: Modular.routerDelegate,
       ),
-      routeInformationParser: Modular.routeInformationParser,
-      routerDelegate: Modular.routerDelegate,
-    );
+);
   }
 }
 
