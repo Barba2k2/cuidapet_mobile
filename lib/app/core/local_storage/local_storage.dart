394e4d7da46d89624681682205e90fd4af0baa87
Barba2k2 <lorenzo.dz@hotmail.com>
2024-05-16 23:31:30 -0300

    Encapsulating Flutter Secure Storage

--- lib/app/core/local_storage/local_storage.dart
+++ lib/app/core/local_storage/local_storage.dart
@@ -6,4 +6,10 @@ abstract class LocalStorage {
   Future<void> remove(String key);
 }
 
-abstract class LocalSecureStorage {}
+abstract class LocalSecureStorage {
+  Future<String?> read(String key);
+  Future<void> write(String key, String value);
+  Future<bool> contains(String key);
+  Future<void> clear();
+  Future<void> remove(String key);
+}
--- lib/app/modules/core/core_module.dart
+++ lib/app/modules/core/core_module.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_modular/flutter_modular.dart';
 
 import '../../core/local_storage/local_storage.dart';
+import '../../core/local_storage/secure_storage/flutter_secure_storage_local_storage_impl.dart';
 import '../../core/local_storage/shared_preferences/shared_preferences_local_storage_impl.dart';
 import '../../core/logger/app_logger.dart';
 import '../../core/logger/logger_app_logger_impl.dart';
@@ -25,6 +26,10 @@ class CoreModule extends Module {
         ),
         Bind.lazySingleton<LocalStorage>(
           (i) => SharedPreferencesLocalStorageImpl(),
+          export: true,
+        ),
+        Bind.lazySingleton<LocalSecureStorage>(
+          (i) => FlutterSecureStorageLocalStorageImpl(),
           export: true,
         ),
       ];
